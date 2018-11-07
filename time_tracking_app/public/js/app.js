2227f9450dcf5954388f8c3099c95c81f35d0e25
Trey Decker <treydecker75@gmail.com>
2018-11-06 22:00:59 -0700

    wired app to server and api

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,22 +1,19 @@
 class TimersDashboard extends React.Component {
     state = {
-        timers: [
-            {
-                title: 'Practice squat',
-                project: 'Gym Chores',
-                id: uuid.v4(),
-                elapsed: 5456099,
-                runningSince: Date.now(),
-            },
-            {
-                title: 'Bake Squash',
-                project: 'Kitchen Chores',
-                id: uuid.v4(),
-                elapsed: 1273998,
-                runningSince: null,
-            },
-        ],
+        timers: [],
     };
+
+    componentDidMount() {
+        this.loadTimersFromServer();
+        setInterval(this.loadTimersFromServer, 5000);
+    };
+
+    loadTimersFromServer = () => {
+        client.getTimers((serverTimers) => (
+            this.setState({ timers: serverTimers })
+            )
+        );
+    }
 
     handleCreateFormSubmit = (timer) => {
         this.createTimer(timer);
@@ -43,11 +40,17 @@ class TimersDashboard extends React.Component {
         this.setState({
             timers: this.state.timers.concat(t),
         });
+
+        client.createTimer(t);
     };
 
     deleteTimer = (timerId) => {
         this.setState({
             timers: this.state.timers.filter((timer) => timer.id !== timerId),
+        });
+
+        client.deleteTimer({
+            id: timerId
         });
     };
 
@@ -64,6 +67,10 @@ class TimersDashboard extends React.Component {
                     return timer;
                 }
             }),
+        });
+
+        client.startTimer({
+            id: timerId, start: now,
         });
     };
 
@@ -83,6 +90,10 @@ class TimersDashboard extends React.Component {
                 }
             }),
         });
+
+        client.stopTimer({
+            id: timerId, stop: now,
+        });
     };
 
     updateTimer = (attrs) => {
@@ -101,7 +112,8 @@ class TimersDashboard extends React.Component {
                 }
             }),
         });
-        console.log('this.state.times', this.state.timers);
+
+        client.updateTimer(attrs);
     };
 
     render() {
